e5fab8575f41ae900bf4b653bef0402d2183d050
mgmonteleone-admin <mgm@aut-aut.hr>
2015-01-12 04:37:06 +0100

    added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='memcacher',
+    version='1.0.0',
+    packages=['libs'],
+    url='www.aut-aut.hr',
+    license='Apache',
+    author='Matthew G. Monteleone',
+    author_email='mgm@aut-aut.hr',
+    description=''
+)
