26b5a649cb89732f499d2d19f021c25cd1a35034
mgmonteleone-admin <mgm@aut-aut.hr>
2015-01-12 05:37:00 +0100

    updated help

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
     name='memcacher',
     version='1.0.0',
-    packages=['libs','log'],
+    packages=['libs'],
     url='www.aut-aut.hr',
     license='Apache',
     author='Matthew G. Monteleone',
     author_email='mgm@aut-aut.hr',
-    description=''
+    description='Command line utility for putting web resources into memcached for nginx usage'
 )
